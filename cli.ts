51493862d0edbb518d384cf8b2a1e586d2a67513
CooldudeNo13026 <86321195+CooldudeNo13026@users.noreply.github.com>
2022-08-07 16:43:10 +0300

    Did some refactoring

--- cli.ts
+++ cli.ts
@@ -1,51 +1,94 @@
 import { getSongFileURL, getTitle, getMetaTags } from ".";
 import sanitize from "sanitize-filename";
+import pkg from "./package.json";
+import minimist from "minimist";
 import download from "download";
 
 (async () => {
-	const [, , mode, id] = process.argv;
+	const args = minimist(process.argv.slice(2));
 
-	const help = () =>
-		console.log(`Usage: 
-ngdl -d | --download <id: number>
-ngdl -i | --info <id: number>
-ngdl -v | --version
-ngdl -h | --help`);
+	const e = process.exit;
 
-	if (mode === "-d" || mode === "--download") {
-		const OUT_DIR =
-			process.platform === "win32"
-				? `${process.env.USERPROFILE}\\Downloads`
-				: `${process.env.HOME}/ngdl`;
+	const OUT_DIR =
+		process.platform === "win32"
+			? `${process.env.USERPROFILE}\\Downloads`
+			: `${process.env.HOME}/ngdl`;
 
-		if (!id) {
-			console.log("No ID Provided");
-			process.exit(1);
-		}
-
-		const url = await getSongFileURL(parseInt(id));
-		const name = await getTitle(parseInt(id));
+	async function downloadAudio(id: number) {
+		const url = await getSongFileURL(id);
+		const name = await getTitle(id);
 
 		await download(url, OUT_DIR, {
-			filename: `${sanitize(name) || "file"}.mp3`,
+			filename: `${sanitize(name) || id.toString()}.mp3`,
 		});
-	} else if (mode === "-v" || mode === "--version") {
-		console.log(`ngdl v${require("../package.json").version}`);
-	} else if (mode === "-i" || mode === "--info") {
-		const info = await getMetaTags(parseInt(id));
+	}
 
+	async function getAudioInfo(id: number) {
+		const info = await getMetaTags(id);
 		const TITLE = info["og:title"];
 		const DESCRIPTION = info["og:description"];
 		const URL = info["og:url"];
-
 		console.log(`Title: ${TITLE}
 Description: ${DESCRIPTION}
 URL: ${URL}`);
-	} else if (mode === "-h" || mode === "--help") {
+	}
+
+	const help = () =>
+		console.log(`Usage:
+ngdl -d <id: number>
+ngdl -i <id: number>
+ngdl -v | --version
+ngdl -h | --help`);
+
+	if (args.h || args.help) {
 		help();
-	} else {
-		console.log("No Mode Provided Or Mode Not Recognized");
-		help();
-		process.exit(1);
+		e(0);
+	}
+
+	if (args.v || args.version) {
+		console.log(`ngdl version ${pkg.version}`);
+		e(0);
+	}
+
+	if (args.d) {
+		if (!(args.d instanceof Array) || typeof args.d !== "string") {
+			console.log("Error: Did Not Receive String Or Array");
+			e(1);
+		}
+		if (typeof args.d === "number") {
+			await downloadAudio(args.d);
+			e(0);
+		}
+		if (typeof args.d === "string") {
+			await downloadAudio(parseInt(args.d));
+			e(0);
+		}
+		(args.d as any[]).forEach(async (id) => {
+			if (typeof id !== "number" || typeof id !== "string") e(1);
+			if (typeof id === "string") await downloadAudio(parseInt(id));
+			if (typeof id === "number") await downloadAudio(id);
+		});
+		e(0);
+	}
+
+	if (args.i) {
+		if (!(args.i instanceof Array) || typeof args.i !== "string") {
+			console.log("Error: Did Not Receive String Or Array");
+			e(1);
+		}
+		if (typeof args.i === "number") {
+			await getAudioInfo(args.i);
+			e(0);
+		}
+		if (typeof args.i === "string") {
+			await getAudioInfo(parseInt(args.i));
+			e(0);
+		}
+		(args.i as any[]).forEach(async (id) => {
+			if (typeof id !== "number" || typeof id !== "string") e(1);
+			if (typeof id === "string") await getAudioInfo(parseInt(id));
+			if (typeof id === "number") await getAudioInfo(id);
+		});
+		e(0);
 	}
 })();
