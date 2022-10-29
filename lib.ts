b8d1cd7c25d8e9eca04d7f6400bca366acff703f
CooldudeNo13026 <86321195+CooldudeNo13026@users.noreply.github.com>
2022-10-29 18:27:52 +0300

    did some refactoring

--- lib.ts
+++ lib.ts
@@ -1,96 +1,38 @@
 import { JSDOM } from "jsdom";
 import axios from "axios";
 
-async function _getData(id: number) {
-	return (await axios.get(`https://newgrounds.com/audio/listen/${id}`))
-		.data as string;
+async function _getDocument(id: number) {
+	const req = await axios.get(`https://newgrounds.com/audio/listen/${id}`);
+	const dom = new JSDOM(req.data);
+	return dom.window.document;
 }
 
-/**
- * Gets The Title Of A Song In Newgrounds
- * @param id {number}
- */
 export async function getTitle(id: number) {
-	const data = await _getData(id);
-
-	const { document } = new JSDOM(data).window;
+	const document = await _getDocument(id);
 
 	return document.title;
 }
 
-/**
- * Gets All The Link Tags In A Newgrounds Song Page
- * @param id {number}
- */
-export async function getLinkTags(id: number) {
-	const data = await _getData(id);
-
-	const { document } = new JSDOM(data).window;
-
-	const linkTags = Array.from(document.head.querySelectorAll("link")).map(
-		(link) => {
-			const sizes = link.attributes.getNamedItem("sizes")?.value;
-
-			return {
-				rel: link.rel,
-				href: link.href,
-				sizes:
-					link.rel === "apple-touch-icon" && sizes ? { sizes } : "",
-			};
-		}
-	);
-
-	return linkTags;
-}
-
-/**
- * Gets All The Meta Tags In A Newgrounds Song Page
- * @param id {number}
- */
 export async function getMetaTags(id: number) {
-	const data = await _getData(id);
-
-	const { document } = new JSDOM(data).window;
+	const document = await _getDocument(id);
 
 	const metaArray = Array.from(document.head.querySelectorAll("meta")).map(
-		(meta) => {
+		(meta, idx) => {
 			const name =
 				meta.name ||
 				meta.attributes.getNamedItem("property")?.value ||
-				meta.httpEquiv;
+				meta.httpEquiv ||
+				idx.toString();
 
-			const value = meta.content;
-
-			return { [name]: value };
+			return { [name]: meta.content };
 		}
 	);
 
 	return metaArray.reduce((total, current) => {
-		const [k, v] = Object.entries(current)[0];
-
-		return { ...total, [k]: v };
+		return { ...total, ...current };
 	}, {});
 }
 
-/**
- * Gets The Url Of A Song File In Newgrounds
- * @param id {number}
- */
 export async function getSongFileURL(id: number) {
-	const data = await _getData(id);
-
-	const { document } = new JSDOM(data).window;
-
-	const script = Array.from(document.querySelectorAll("script"))
-		.map((script) => script.innerHTML)
-		.map((script) => script.replace(/\n/g, "\n").replace(/\t/g, "\t"))
-		.filter((script) => script !== undefined || script !== "")
-		.filter((script) =>
-			script.includes("var embed_controller = new embedController")
-		)[0]
-		.replace(/var embed_controller = new embedController\(\[\{"url":"/g, "")
-		.split('","is_published"')[0]
-		.replace(/\\\//g, "/");
-
-	return script;
+	return `https://newgrounds.com/audio/download/${id}`;
 }
